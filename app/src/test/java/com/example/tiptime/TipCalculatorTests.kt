53b6168acd65fc635e6d52f31f94f7b7f863055e
Trevo <trevoc33@gmail.com>
2023-08-15 13:49:18 +0100

    Desktop Computer - local test passed

--- app/src/test/java/com/example/tiptime/TipCalculatorTests.kt
+++ app/src/test/java/com/example/tiptime/TipCalculatorTests.kt
@@ -1,11 +1,16 @@
 package com.example.tiptime
 
 import org.junit.Test
+import java.text.NumberFormat
+import org.junit.Assert.assertEquals
 
 class TipCalculatorTests {
     @Test
     fun calculateTip_20PercentNoRoundup(){
         val amount = 10.00
         val tipPercent = 20.00
+        val expectedTip = NumberFormat.getCurrencyInstance().format(2)
+        val actualTip = calculateTip(amount,tipPercent,roundUp = false)
+        assertEquals(expectedTip,actualTip)
     }
 }
